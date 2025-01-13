fd0ca1f7d5f24c865ab4797f277b192cc99d7f05
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2025-01-13 19:44:50 +0300

    learning state

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <Message />
       <ListGroup />
+      <ListGroup />
     </div>
   );
 }
--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
+
 const ListGroup = () => {
   let cities = ["New york", "D.C", "Los Anglos", "Cairo"];
+
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>List</h1>
       <div>
         <ul className="list-group">
-          {cities.map((item) => (
-            <li key={item} className="list-group-item">
+          {cities.map((item, index) => (
+            <li
+              className={
+                selectedIndex === index
+                  ? "list-group-item active"
+                  : "list-group-item"
+              }
+              key={item}
+              onClick={() => {
+                setSelectedIndex(index);
+              }}
+            >
               {item}
             </li>
           ))}
